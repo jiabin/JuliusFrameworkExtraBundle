178a8a9416633e24f71229bb413be3ba5defd5d1
Eymen Gunay <eymen@egunay.com>
2013-06-30 19:04:33 +0200

    [Minor] Code cleanup

--- Form/Type/DateTimePickerType.php
+++ Form/Type/DateTimePickerType.php
@@ -23,13 +23,6 @@ use Symfony\Component\Form\AbstractType;
 
 class DateTimePickerType extends AbstractType
 {
-    private static $acceptedFormats = array(
-        \IntlDateFormatter::FULL,
-        \IntlDateFormatter::LONG,
-        \IntlDateFormatter::MEDIUM,
-        \IntlDateFormatter::SHORT,
-    );
-
     /**
      * {@inheritdoc}
      */
@@ -50,20 +43,10 @@ class DateTimePickerType extends AbstractType
             }
 
             return array(
-                'year' => $emptyValue,
+                'year'  => $emptyValue,
                 'month' => $emptyValue,
-                'day' => $emptyValue
+                'day'   => $emptyValue
             );
-        };
-
-        // BC until Symfony 2.3
-        $modelTimezone = function (Options $options) {
-            return $options['data_timezone'];
-        };
-
-        // BC until Symfony 2.3
-        $viewTimezone = function (Options $options) {
-            return $options['user_timezone'];
         };
 
         $resolver->setDefaults(array(
@@ -86,44 +69,44 @@ class DateTimePickerType extends AbstractType
             //  MM : full textual representation of a month, such as January or March
             //  yy : two digit representation of a year
             //  yyyy : full numeric representation of a year, 4 digits
-            'format' => 'yyyy-M-dd hh:ii',
+            'format'                => 'yyyy-mm-dd hh:ii',
             // Day of the week start. 0 (Sunday) to 6 (Saturday)
-            'weekStart' => 0,
+            'weekStart'             => 0,
             // Days of the week that should be disabled. Values are 0 (Sunday) to 6 (Saturday)
-            'daysOfWeekDisabled' => array(),
+            'daysOfWeekDisabled'    => array(),
             // Whether or not to close the datetimepicker immediately when a date is selected.
-            'autoclose' => true,
+            'autoclose'             => true,
             // The view that the datetimepicker should show when it is opened. Accepts values of :
             //  0 or 'hour' for the hour view
             //  1 or 'day' for the day view
             //  2 or 'month' for month view (the default)
             //  3 or 'year' for the 12-month overview
             //  4 or 'decade' for the 10-year overview. Useful for date-of-birth datetimepickers.
-            'startView' => 2,
+            'startView'             => 2,
             // The lowest view that the datetimepicker should show.
-            'minView' => 0,
+            'minView'               => 0,
             // The highest view that the datetimepicker should show.
-            'maxView' => 4,
+            'maxView'               => 4,
             // If true or "linked", displays a "Today" button at the bottom of the datetimepicker to select the current date
             // If true, the "Today" button will only move the current date into view; if "linked", the current date will also be selected.
-            'todayBtn' => true,
+            'todayBtn'              => true,
             // If true, highlights the current date.
-            'todayHighlight' => true,
+            'todayHighlight'        => true,
             // Whether or not to allow date navigation by arrow keys.
-            'keyboardNavigation' => true,
+            'keyboardNavigation'    => true,
             // The two-letter code of the language to use for month and day names. 
             // These will also be used as the input's value (and subsequently sent to the server in the case of form submissions). 
             // Currently ships with English ('en'), German ('de'), Brazilian ('br'), and Spanish ('es') translations, 
             // but others can be added (see I18N below). If an unknown language code is given, English will be used.
-            'language' => 'en',
+            'language'              => 'en',
             // The increment used to build the hour view. A preset is created for each minuteStep minutes.
-            'minuteStep' => 5,
+            'minuteStep'            => 5,
             // Allowed values are bottom-right and bottom-left
-            'pickerPosition' => 'bottom-right',
+            'pickerPosition'        => 'bottom-right',
             // This option will enable meridian views for day and hour views.
-            'showMeridian' => false,
-
-            'empty_value' => $emptyValue,
+            'showMeridian'          => false,
+            // Empty value
+            'empty_value'           => $emptyValue,
         ));
 
         $resolver->setNormalizers(array(
@@ -138,12 +121,7 @@ class DateTimePickerType extends AbstractType
     {
         $format = 'Y-m-d H:i';
 
-        $builder->addViewTransformer(new DateTimeToStringTransformer(
-            null,
-            null,
-            $format,
-            false
-        ));
+        $builder->addViewTransformer(new DateTimeToStringTransformer(null, null, $format, false));
     }
 
     /**
@@ -153,20 +131,20 @@ class DateTimePickerType extends AbstractType
     {
         parent::finishView($view, $form, $options);
 
-        $view->vars['format'] = $options['format'];
-        $view->vars['weekStart'] = $options['weekStart'];
-        $view->vars['daysOfWeekDisabled'] = $options['daysOfWeekDisabled'];
-        $view->vars['autoclose'] = $options['autoclose'] === true ? 'true' : 'false';
-        $view->vars['startView'] = $options['startView'];
-        $view->vars['minView'] = $options['minView'];
-        $view->vars['maxView'] = $options['maxView'];
-        $view->vars['todayBtn'] = $options['todayBtn'] === true ? 'true' : 'false';
-        $view->vars['todayHighlight'] = $options['todayHighlight'] === true ? 'true' : 'false';
-        $view->vars['keyboardNavigation'] = $options['keyboardNavigation'] === true ? 'true' : 'false';
-        $view->vars['language'] = $options['language'];
-        $view->vars['minuteStep'] = $options['minuteStep'];
-        $view->vars['pickerPosition'] = $options['pickerPosition'];
-        $view->vars['showMeridian'] = $options['showMeridian'] === true ? 'true' : 'false';
+        $view->vars['format']               = $options['format'];
+        $view->vars['weekStart']            = $options['weekStart'];
+        $view->vars['daysOfWeekDisabled']   = $options['daysOfWeekDisabled'];
+        $view->vars['autoclose']            = $options['autoclose'] === true ? 'true' : 'false';
+        $view->vars['startView']            = $options['startView'];
+        $view->vars['minView']              = $options['minView'];
+        $view->vars['maxView']              = $options['maxView'];
+        $view->vars['todayBtn']             = $options['todayBtn'] === true ? 'true' : 'false';
+        $view->vars['todayHighlight']       = $options['todayHighlight'] === true ? 'true' : 'false';
+        $view->vars['keyboardNavigation']   = $options['keyboardNavigation'] === true ? 'true' : 'false';
+        $view->vars['language']             = $options['language'];
+        $view->vars['minuteStep']           = $options['minuteStep'];
+        $view->vars['pickerPosition']       = $options['pickerPosition'];
+        $view->vars['showMeridian']         = $options['showMeridian'] === true ? 'true' : 'false';
     }
 
     /**
